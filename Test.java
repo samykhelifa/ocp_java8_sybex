8df4dfa5ddea9fd6a90541d6733f7d6601219db2
samy <39716963+samykhelifa@users.noreply.github.com>
2019-09-21 14:29:05 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,12 @@
+import java.io.*;
+// import java.nio.file.* ;
 
+public class Test  {
+
+     public static void main(String[] args) {
+        File f= new File("root/java_io_apress2015/samy.txt");
+      //   FileSystem fs = FileSystems.getDefault();
+          
+          System.out.println(f.exists());
+}
+}
