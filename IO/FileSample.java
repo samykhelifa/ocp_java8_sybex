daeb84f03d41962e3969d7e2c05e49d2b4fe2156
samy <39716963+samykhelifa@users.noreply.github.com>
2019-09-21 15:34:44 +0200

    Create FileSample.java

---
+++ IO/FileSample.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class FileSample {  
+    public static void main(String[] args) {     
+          File f= new File("/root/ocp_java8_sybex/samy.txt");
+          System.out.println(file.exists());   
+          } 
+  }
