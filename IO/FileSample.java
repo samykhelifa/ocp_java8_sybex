9306b6dc90391395b092acd9c0fd2252c2aeda94
samy <39716963+samykhelifa@users.noreply.github.com>
2019-09-21 15:38:24 +0200

    Update FileSample.java

--- IO/FileSample.java
+++ IO/FileSample.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class FileSample {  
     public static void main(String[] args) {     
-          File f= new File("/root/ocp_java8_sybex/samy.txt");
-          System.out.println(file.exists());   
+          File f= new File("/root/ocp_java8_sybex/IO/a.txt");
+          System.out.println(f.exists());   
           } 
   }
