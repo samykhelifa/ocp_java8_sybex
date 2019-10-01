214048d4dc267972c2493e522f61717dd16bf4f0
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-01 11:42:27 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,5 +8,6 @@ public class Test  {
       //   FileSystem fs = FileSystems.getDefault();
           
           System.out.println(f.exists());
+          System.out.println(System.getProperties());
 }
 }
