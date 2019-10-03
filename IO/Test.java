62c03d71953c70466f9b55bf8b4a3f59bae076c2
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-03 16:40:42 +0200

    Update Test.java

--- IO/Test.java
+++ IO/Test.java
@@ -1,1 +1,15 @@
+import java.io.*;
+// import java.nio.file.* ;
 
+public class Test  {
+
+     public static void main(String[] args) {
+        File f= new File("/root/ocp_java8_sybex/samy.txt");
+      //   FileSystem fs = FileSystems.getDefault();
+          
+          System.out.println(f.exists());
+          Enumeration<?>  en =System.getProperties().propertyNames();
+           while( en.hasMoreElements() )
+           System.out.println(en.nextElement());
+}
+}
