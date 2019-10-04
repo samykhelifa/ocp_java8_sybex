226da6aee14e0f91769d33ffb4a8b427e58fdda4
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-04 02:29:54 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,12 +4,15 @@ import java.io.*;
 public class Test  {
 
      public static void main(String[] args) {
-        File f= new File("/root/ocp_java8_sybex/samy.txt");
+        File f = new File("/root/ocp_java8_sybex/a.txt");
       //   FileSystem fs = FileSystems.getDefault();
           
           System.out.println(f.exists());
           Enumeration<?>  en =System.getProperties().propertyNames();
            while( en.hasMoreElements() )
            System.out.println(en.nextElement());
+          System.out.println(System.getProperty("file.separator"));
+          System.out.println(java.io.File.separator);
+          
 }
 }
